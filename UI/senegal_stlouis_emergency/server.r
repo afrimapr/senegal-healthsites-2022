737b899070bdf76af0fd8cca22a212bd3878c948
AndySouth <southandy@gmail.com>
2022-03-24 19:12:04 +0000

    modify to display recent data

--- UI/senegal_stlouis_emergency/server.r
+++ UI/senegal_stlouis_emergency/server.r
@@ -18,15 +18,14 @@ library(sf)
 
 #load the data
 #temporarily put it in extdata
-filename <- "St-Louis-Data-Collection-Campaign-Gth_Hs_Osm_11.csv"
-filename <- system.file("extdata", filename, package = "afrihealthsites")
+# filename <- "St-Louis-Data-Collection-Campaign-Gth_Hs_Osm_11.csv"
+# filename <- system.file("extdata", filename, package = "afrihealthsites")
+# # encoding="UTF-8" fixes accent problems
+# dfsen <- utils::read.csv(filename, stringsAsFactors = FALSE, encoding="UTF-8")
+# sfsen <- sf::st_as_sf(dfsen, coords=c("Facility.Location...longitude", "Facility.Location...latitude"), crs = 4326, na.fail = FALSE)
 
-# encoding="UTF-8" fixes accent problems
-dfsen <- utils::read.csv(filename, stringsAsFactors = FALSE, encoding="UTF-8")
 
-#BEWARE of order of long lat and it changing with sf version
-#sfsen <- sf::st_as_sf(dfsen, coords=c("Facility.Location...latitude","Facility.Location...longitude"))
-sfsen <- sf::st_as_sf(dfsen, coords=c("Facility.Location...longitude", "Facility.Location...latitude"), crs = 4326, na.fail = FALSE)
+readRDS("sfosm.RDS")
 
 
 # Define a server for the Shiny app
@@ -36,16 +35,21 @@ function(input, output) {
   output$serve_healthsites_map <- renderLeaflet({
 
     #if attribute column is numeric use it to set symbol size, otherwise keep symbol size constant
-    cex <- ifelse(is.numeric(sfsen[[input$attribute_to_plot]]),input$attribute_to_plot,5)
+    # cex <- ifelse(is.numeric(sfsen[[input$attribute_to_plot]]),input$attribute_to_plot,5)
 
-    mapplot <- mapview::mapview(sfsen,
-                     zcol = input$attribute_to_plot,
-                     layer.name = input$attribute_to_plot,
-                     label=paste(sfsen[["Facility.Category"]],":",sfsen[["Name.of.Facility"]],":",sfsen[[input$attribute_to_plot]]),
-                     #at = c(0,1,2,5,10,50,100,250),
-                     #at = c(0,1,2,10,50,100,250),
-                     cex = cex)
+    # mapplot <- mapview::mapview(sfsen,
+    #                  zcol = input$attribute_to_plot,
+    #                  layer.name = input$attribute_to_plot,
+    #                  label=paste(sfsen[["Facility.Category"]],":",sfsen[["Name.of.Facility"]],":",sfsen[[input$attribute_to_plot]]),
+    #                  #at = c(0,1,2,5,10,50,100,250),
+    #                  #at = c(0,1,2,10,50,100,250),
+    #                  cex = cex)
 
+    mapplot <- mapview::mapview(sfosm,
+                                zcol="emergency_pregnancy",
+                                label=paste("osm:",sfosm[["name_of_facility"]]),
+                                col.regions = c('lightblue','red')) 
+        
     #important that this returns the @map bit
     #otherwise get Error in : $ operator not defined for this S4 class
     mapplot@map
