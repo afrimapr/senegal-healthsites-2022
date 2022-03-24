65361ecfe3e1acf451039fba4e737b6c36144254
AndySouth <southandy@gmail.com>
2022-03-24 18:53:31 +0000

    copy previous shiny Senegal app from afrihealthsites

---
+++ UI/senegal_stlouis_emergency/server.r
@@ -0,0 +1,57 @@
+#afrihealthsites/senegal_healthsites/server.r
+# keeping this very simple partly so it can be used as a template by other (maybe new) R users
+
+
+cran_packages <- c("leaflet","remotes")
+lapply(cran_packages, function(x) if(!require(x,character.only = TRUE)) install.packages(x))
+
+library(remotes)
+library(leaflet)
+
+# if(!require(afrihealthsites)){
+#   remotes::install_github("afrimapr/afrihealthsites")
+# }
+# 
+# library(afrihealthsites)
+library(mapview)
+library(sf)
+
+#load the data
+#temporarily put it in extdata
+filename <- "St-Louis-Data-Collection-Campaign-Gth_Hs_Osm_11.csv"
+filename <- system.file("extdata", filename, package = "afrihealthsites")
+
+# encoding="UTF-8" fixes accent problems
+dfsen <- utils::read.csv(filename, stringsAsFactors = FALSE, encoding="UTF-8")
+
+#BEWARE of order of long lat and it changing with sf version
+#sfsen <- sf::st_as_sf(dfsen, coords=c("Facility.Location...latitude","Facility.Location...longitude"))
+sfsen <- sf::st_as_sf(dfsen, coords=c("Facility.Location...longitude", "Facility.Location...latitude"), crs = 4326, na.fail = FALSE)
+
+
+# Define a server for the Shiny app
+function(input, output) {
+
+  # mapview interactive leaflet map plot
+  output$serve_healthsites_map <- renderLeaflet({
+
+    #if attribute column is numeric use it to set symbol size, otherwise keep symbol size constant
+    cex <- ifelse(is.numeric(sfsen[[input$attribute_to_plot]]),input$attribute_to_plot,5)
+
+    mapplot <- mapview::mapview(sfsen,
+                     zcol = input$attribute_to_plot,
+                     layer.name = input$attribute_to_plot,
+                     label=paste(sfsen[["Facility.Category"]],":",sfsen[["Name.of.Facility"]],":",sfsen[[input$attribute_to_plot]]),
+                     #at = c(0,1,2,5,10,50,100,250),
+                     #at = c(0,1,2,10,50,100,250),
+                     cex = cex)
+
+    #important that this returns the @map bit
+    #otherwise get Error in : $ operator not defined for this S4 class
+    mapplot@map
+
+
+    })
+
+
+}
---
+++ UI/senegal_stlouis_emergency/ui.r
@@ -0,0 +1,54 @@
+#/ui.r
+
+# cran_packages <- c("shiny","leaflet","remotes")
+# lapply(cran_packages, function(x) if(!require(x,character.only = TRUE)) install.packages(x))
+
+
+library(shiny)
+library(leaflet)
+library(remotes)
+
+# if(!require(afrihealthsites)){
+#   remotes::install_github("afrimapr/afrihealthsites")
+# }
+# 
+# library(afrihealthsites)
+
+library(mapview)
+
+
+pageWithSidebar(
+  headerPanel('Senegal St Louis emergency care pilot viewer'),
+  sidebarPanel( width=2,
+
+    # checkboxGroupInput("hs_amenity", label = h5("healthsites amenities"),
+    #                    choices = list("hospital"="hospital", "clinic"="clinic", "doctors"="doctors", "pharmacy"="pharmacy", "unlabelled"="", "dentist" = "dentist"),
+    #                    selected = c("hospital","clinic","doctors","pharmacy")),
+
+    # radioButtons("attribute_to_plot", label = h3("Attribute to plot"),
+    #              choices = list("Number.of.Beds" = "Number.of.Beds",
+    #                             "Number.of.Doctors" = "Number.of.Doctors",
+    #                             "Number.of.Nurses"= "Number.of.Nurses",
+    #                             "Operator.Type" = "Operator.Type",
+    #                             "When.is.the.facility.open." = "When.is.the.facility.open.",
+    #                             "Facility.Category" = "Facility.Category",
+    #                             "Does.this.facility.provide.Emergency.Services." = "Does.this.facility.provide.Emergency.Services.",
+    #                             "Types.of.insurance.accepted." = "Types.of.insurance.accepted."),
+    #              selected = "Number.of.Beds"),
+
+    p("PROTOTYPE under development March 2022\n"),
+
+    p("Developed by ", a("afrimapr", href="http://www.afrimapr.org", target="_blank"), "Contact : ", a("@southmapr", href="https://twitter.com/southmapr", target="_blank")),
+    p("Open source ", a("R code", href="https://github.com/afrimapr/senegal-healthsites-2022", target="_blank")),
+
+    p("Input and suggestions ", a("welcome", href="https://github.com/afrimapr/suggestions_and_requests", target="_blank")),
+
+    p(tags$small("Disclaimer : Data used by afrimapr are sourced from published open data sets. We provide no guarantee of accuracy.")),
+
+  ),
+  mainPanel(
+    leafletOutput("serve_healthsites_map", height=1000)
+  )
+)
+
+
